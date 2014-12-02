15f9a71d8acd86a195890acb97ff9660af7f364a
John Vandenberg <jayvdb@gmail.com>
2014-12-02 21:47:31 +0700

    Fix claimit -exists functionality
    The implementation of -exists meant options 's' and 'q' were not
    operative, as it did not set any sources or qualifiers on the claim
    to be added, yet it test the missing sources and qualifiers against
    those already on the item.  As a consequence, 's' and 'q' would only
    prevent a new claim being added when the item had no sources or
    qualifiers respectively, as then both sides of the equality test
    would be empty and therefore equivalent.
    
    Also, the utility method which did the comparison did not understand
    the structure of Claim.sources, and did not test equality correctly.
    
    This change slightly re-interprets the meaning of 's' and 'q',
    so that 's' means 'allow adding claims without a source' and 'allow
    adding claims even if a claim on the same property has the same source'
    and 'q' means 'allow adding claims even if the property has a claim
    with a qualifier.'
    
    Bug: T69284
    Change-Id: I4c1c0b8b786ceb116497b192b8167cf8f63dc30b

--- scripts/claimit.py
+++ scripts/claimit.py
@@ -35,7 +35,7 @@ python claimit.py [pagegenerators] P246 "string example" -exists:p
 
 Suppose the claim you want to add has the same property as an existing claim
 and the "-exists:p" argument is used. Now, claimit.py will not add the claim
-if it has the same target, sources, and/or qualifiers as the existing claim.
+if it has the same target, source, and/or the existing claim has qualifiers.
 To override this behavior, add 't' (target), 's' (sources), or 'q' (qualifiers)
 to the 'exists' argument.
 
@@ -57,8 +57,20 @@ but 'p' must be included.
 __version__ = '$Id$'
 #
 
+import copy
+import sys
+
 import pywikibot
+
 from pywikibot import pagegenerators, WikidataBot
+
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
+
+if sys.version_info[0] > 2:
+    unicode = str
 
 # This is required for the text that is shown when you run this script
 # with the parameter -help or without parameters.
@@ -66,71 +78,148 @@ docuReplacements = {
     '&params;': pagegenerators.parameterHelp,
 }
 
+EXISTS_HELP_MSG = ("Append '%s' to the -exists: argument "
+                   "to override this behavior.")
+
 
 class ClaimRobot(WikidataBot):
 
-    """A bot to add Wikidata claims."""
+    """
+    A bot to add Wikidata claims with a client page as a source.
 
-    def __init__(self, generator, claims, exists_arg=''):
+    FIXME: Only the claim target and page source are properly checked in the
+    'exists' algorithm.  Qualifiers and other sources on the claims are added,
+    but are not used in the 'exists' checking algorithm. T76547.
+    """
+
+    def __init__(self, generator, claims, exists_arg=None):
         """
         Constructor.
 
-        Arguments:
-            * generator    - A generator that yields Page objects.
-            * claims       - A list of wikidata claims
-            * exists_arg   - String specifying how to handle duplicate claims
+        @param generator: A generator that yields Page objects.
+        @param claims: A list of wikidata claims
+        @param exists_arg: String specifying how to handle duplicate claims
 
         """
         super(ClaimRobot, self).__init__()
         self.generator = generator
         self.claims = claims
-        self.exists_arg = exists_arg
         self.cacheSources()
-        if self.exists_arg:
-            pywikibot.output('\'exists\' argument set to \'%s\'' % self.exists_arg)
+        if not exists_arg:
+            self.exists_arg = set()
+        else:
+            self.exists_arg = set(exists_arg)
+            pywikibot.output('\'exists\' argument set to \'%s\''
+                             % ','.join(sorted(self.exists_arg)))
+            if set(['s', 'q']) & self.exists_arg:
+                for claim in self.claims:
+                    if claim.sources or claim.qualifiers:
+                        pywikibot.warning("%s 'exists' detection does not support claims with sources or qualifiers.")
 
     def treat(self, page, item):
         """Treat each page."""
         self.current_page = page
 
-        if item:
-            for claim in self.claims:
-                skip = False
-                # If claim with same property already exists...
-                if claim.getID() in item.claims:
-                    if self.exists_arg is None or 'p' not in self.exists_arg:
-                        pywikibot.log('Skipping %s because claim with same property already exists' % (claim.getID(),))
-                        pywikibot.log('Use the -exists:p option to override this behavior')
-                        skip = True
-                    else:
-                        existing_claims = item.claims[claim.getID()]  # Existing claims on page of same property
-                        for existing in existing_claims:
-                            skip = True  # Default value
-                            # If some attribute of the claim being added matches some attribute in an existing claim
-                            # of the same property, skip the claim, unless the 'exists' argument overrides it.
-                            if claim.getTarget() == existing.getTarget() and 't' not in self.exists_arg:
-                                pywikibot.log('Skipping %s because claim with same target already exists' % (claim.getID(),))
-                                pywikibot.log('Append \'t\' to the -exists argument to override this behavior')
-                                break
-                            if listsEqual(claim.getSources(), existing.getSources()) and 's' not in self.exists_arg:
-                                pywikibot.log('Skipping %s because claim with same sources already exists' % (claim.getID(),))
-                                pywikibot.log('Append \'s\' to the -exists argument to override this behavior')
-                                break
-                            if listsEqual(claim.qualifiers, existing.qualifiers) and 'q' not in self.exists_arg:
-                                pywikibot.log('Skipping %s because claim with same qualifiers already exists' % (claim.getID(),))
-                                pywikibot.log('Append \'q\' to the -exists argument to override this behavior')
-                                break
-                            skip = False
-                if not skip:
-                    pywikibot.output('Adding %s --> %s'
-                                     % (claim.getID(), claim.getTarget()))
-                    item.addClaim(claim)
-                    # A generator might yield pages from multiple languages
-                    source = self.getSource(page.site)
-                    if source:
-                        claim.addSource(source, bot=True)
-                    # TODO FIXME: We need to check that we aren't adding a
-                    # duplicate
+        if not item:
+            return
+
+        # A generator might yield pages from multiple languages
+        source = self.getSource(page.site)
+
+        # getSource may not know about the site of this page.
+        if not source and 's' not in self.exists_arg:
+            pywikibot.log(
+                ('Skipping %s because the source could not be determined for '
+                 '%s.\n' + EXISTS_HELP_MSG)
+                % (item.title(), page.title(), 's'))
+            return
+
+        for claim in self.claims:
+            # Clone, so attributes modified are not retained.
+            claim = copy.deepcopy(claim)
+
+            if source:
+                claim.sources.append(
+                    OrderedDict({source.getID(): [source]}))
+
+            # Check if claim with same property is desired
+            exists_result = self.check_exists(item, claim)
+            if exists_result:
+                pywikibot.output(
+                    'Failed exists checks. Use -exists:%s to override.'
+                    % ''.join(sorted(self.exists_arg | exists_result)))
+            else:
+                pywikibot.output('Adding %s --> %s'
+                                 % (claim.getID(), claim.getTarget()))
+
+                item.addClaim(claim)
+                if source:
+                    claim.addSource(source)
+
+    def check_exists(self, item, claim):
+        """
+        Check the exists options to determine whether additional claim allowed.
+
+        @param item: The item to add the claim to
+        @type item: ItemPage
+        @param claim: A potential claim to add to item with the same property
+        @type claim: Claim
+        @rtype: True or set of exists flags
+        """
+        if 'p' not in self.exists_arg:
+            if claim.getID() in item.claims:
+                pywikibot.log('Skipping %s because claim with same property already exists' % (claim.getID(),))
+                pywikibot.log('Use the -exists:p option to override this behavior')
+                return set(['p'])
+            else:
+                return set()
+
+        existing_claims = item.claims[claim.getID()]
+
+        # Checking existing claims of same property on the item
+        exists_result = set()
+        dup = False
+        for existing in existing_claims:
+            # If some attribute of the claim being added matches some
+            # attribute in an existing claim of the same property, skip
+            # the claim, unless the 'exists' argument overrides it.
+            if claim.getTarget() == existing.getTarget():
+                dup = True
+                if 't' not in self.exists_arg:
+                    pywikibot.log('Skipping %s because claim with same target already exists' % (claim.getID(),))
+                    pywikibot.log('Append \'t\' to the -exists argument to override this behavior')
+                    exists_result.add('t')
+
+            # FIXME: exists options 's' and 'q' only work correctly for the
+            # page source and no qualifiers respectively, which is all the
+            # command line parameters currently permit.
+
+            # The claim may already have sources if instantiated from another
+            # script.  If run via the command line, the claim has no sources,
+            # other than the last item in sources which is the is page.site.
+
+            if 's' not in self.exists_arg and not existing.getSources():
+                pywikibot.log('Skipping %s because claim without source already exists' % (claim.getID(),))
+                pywikibot.log('Append \'s\' to the -exists argument to override this behavior')
+                exists_result.add('s')
+
+            elif ('s' not in self.exists_arg
+                    and claim.sources[-1].values()[0][0].getTarget()
+                       in get_item_targets(existing.getSources())):
+                pywikibot.log('Skipping %s because claim with the same page site already exists' % (claim.getID(),))
+                pywikibot.log('Append \'s\' to the -exists argument to override this behavior')
+                exists_result.add('s')
+
+            # If run via the command line, the claim has no qualifiers.
+            if ('q' not in self.exists_arg and not existing.qualifiers):
+                pywikibot.log('Skipping %s because claim without qualifiers already exists' % (claim.getID(),))
+                pywikibot.log('Append \'q\' to the -exists argument to override this behavior')
+                exists_result.add('q')
+
+        if dup and not exists_result:
+            pywikibot.warning('Adding duplicate value to %s.' % claim.getID())
+
+        return exists_result
 
 
 def listsEqual(list1, list2):
@@ -138,6 +227,8 @@ def listsEqual(list1, list2):
     Return true if the lists are probably equal, ignoring order.
 
     Works for lists of unhashable items (like dictionaries).
+    Does not currently work, as it depends on a __eq__ operator
+    being created for Claim. T76615
     """
     if len(list1) != len(list2):
         return False
@@ -147,6 +238,14 @@ def listsEqual(list1, list2):
         if item not in list2:
             return False
     return True
+
+
+def get_item_targets(sources):
+    """Return a list of all wikibase-item claim targets in sources."""
+    return [claim.getTarget() for source in sources
+            for claims in source.values()
+            for claim in claims
+            if claim.type == 'wikibase-item']
 
 
 def main(*args):
