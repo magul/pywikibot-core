b44995bec00bc76503f57e0ed77d41e409fcb210
Fabian Neundorf <CommodoreFabianus@gmx.de>
2015-06-02 17:33:00 +0200

    [IMPROV] tests: Test for edit conflicts
    Change-Id: Ia3b64c1aeec891566259a37e7263a117c8e902e0

--- tests/edit_tests.py
+++ tests/edit_tests.py
@@ -10,6 +10,7 @@ from __future__ import unicode_literals
 __version__ = '$Id$'
 #
 
+import re
 import time
 
 import pywikibot
@@ -53,6 +54,39 @@ class TestGeneralWrite(TestCase):
         p = pywikibot.Page(self.site, 'User:John Vandenberg/async test write')
         self.assertEqual(p.text, ts)
         self.assertTrue(called_back)
+
+    def test_edit_conflict(self):
+        r"""
+        Test if an edit conflict occurs.
+
+        It uses two Page instances initialized at the same point. One of them
+        then changes the text so that when the other now saves two an edit
+        conflict occurs.
+
+        The page must exist and follow the format '\d+.\d+ #\d'.
+        """
+        previous_page = pywikibot.Page(self.site, 'User:BobBot/Test edit conflict')
+        if not previous_page.exists():
+            raise unittest.SkipTest('The page "{0}" does not exist. Please '
+                                    'create it.'.format(previous_page))
+        content_match = re.match('^\d+.\d+ #(\d)$', previous_page.get())
+        if not content_match:
+            raise unittest.SkipTest('The page "{0}" does not '
+                                    'match.'.format(previous_page))
+        ts = time.time()
+        # At least two of the numbers aren't used
+        contents = ['{0} #{1}'.format(ts, n)
+                    for n in range(1, 4) if n != int(content_match.group(1))]
+
+        previous_page.text = contents[0]
+        conflict_page = pywikibot.Page(self.site, previous_page.title())
+        conflict_page.text = contents[1]
+        conflict_page.save(summary='An intermediate save so that it conflicts.')
+        self.assertRaises(pywikibot.EditConflict, previous_page.save,
+                          summary='This save should not happen as it conflicts')
+        self.assertEqual(previous_page.text, contents[0])
+        self.assertEqual(conflict_page.get(force=True), contents[1])
+
 
 if __name__ == '__main__':
     try:
