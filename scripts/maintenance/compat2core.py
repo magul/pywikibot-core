ab5e92d6647ace485f0ce0b8c4995d96a5ca896c
Ricordisamoa <ricordisamoa@openmailbox.org>
2015-07-19 23:56:05 +0200

    compat2core: use with statement instead of closing files manually
    Also close a file earlier in ConvertBot.warning()
    
    Change-Id: I56fd31f6080535edd00e60af93d816d6e7285b48

--- scripts/maintenance/compat2core.py
+++ scripts/maintenance/compat2core.py
@@ -24,6 +24,7 @@ to show warnings about deprecated methods:
 """
 #
 # (C) xqt, 2014
+# (C) Pywikibot team, 2015
 #
 # Distributed under the terms of the MIT license.
 #
@@ -155,23 +156,21 @@ class ConvertBot(object):
             exit()
 
     def convert(self):
-        f = codecs.open(self.source, "r", "utf-8")
-        text = f.read()
-        f.close()
+        with codecs.open(self.source, "r", "utf-8") as f:
+            text = f.read()
         for r in replacements:
             text = re.sub(r[0], r[1], text)
-        g = codecs.open(self.dest, "w", "utf-8")
-        g.write(text)
-        g.close()
+        with codecs.open(self.dest, "w", "utf-8") as g:
+            g.write(text)
 
     def warning(self):
         filename = self.source if self.warnonly else self.dest
-        g = codecs.open(filename, "r", "utf-8")
-        for i, line in enumerate(g, start=1):
+        with codecs.open(filename, "r", "utf-8") as g:
+            lines = enumerate(g.readlines(), start=1)
+        for i, line in lines:
             for w in warnings:
                 if w[0] in line:
                     pywikibot.warning(u'line %d: %s>>> %s\n' % (i, line, w[1]))
-        g.close()
 
 
 def main():
