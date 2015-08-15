6d60383f7204d95a81e69fce54e60d93769d36c3
xqt <info@gno.de>
2015-08-15 08:16:55 +0200

    [IMPR] UploadEntry class retrieve FilePage for page() method.
    change site.py and page.py accordingly
    
    Change-Id: I84e3db3ace905508659cc23b45cec75bac0e53ab

--- pywikibot/logentries.py
+++ pywikibot/logentries.py
@@ -248,6 +248,17 @@ class UploadEntry(LogEntry):
 
     _expectedType = 'upload'
 
+    def page(self):
+        """
+        FilePage on which action was performed.
+
+        Note: title may be missing in data dict e.g. by oversight action to
+              hide the title. In that case a KeyError exception will raise
+        """
+        if not hasattr(self, '_page'):
+            self._page = pywikibot.FilePage(self.site, self.data['title'])
+        return self._page
+
 
 class MoveEntry(LogEntry):
 
