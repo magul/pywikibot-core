e2a336bf5c9893fd45ed93b11fd2da2049e182c7
Vadiraja K <vadi.fedx@gmail.com>
2015-12-12 19:48:04 +0530

    Add async parameter to ItemPage.editEntity
    'async' parameter enables ItemPage.editEntity to edit an entity asynchronously,
    which can be used in Wikidata bots.
    
    TODO: handle exception?
    
    Bug: T86074
    Change-Id: I1ffa92b8710e91b7095b24808c151ff9d0a592cf

--- pywikibot/page.py
+++ pywikibot/page.py
@@ -3541,7 +3541,7 @@ class WikibasePage(BasePage):
             return site.dbName()
         return site
 
-    def editEntity(self, data=None, **kwargs):
+    def editEntity(self, data=None, async=False, **kwargs):
         """
         Edit an entity using Wikibase wbeditentity API.
 
@@ -3553,7 +3553,13 @@ class WikibasePage(BasePage):
 
         @param data: Data to be saved
         @type data: dict, or None to save the current content of the entity.
+        @param async: if True, launch a separate thread to edit asynchronously
+        @type async: bool
         """
+        if async:
+            pywikibot.async_request(self.editEntity, data, **kwargs)
+            return
+
         if hasattr(self, '_revid'):
             baserevid = self.latest_revision_id
         else:
