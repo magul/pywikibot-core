ff1ca8a8f87fa890554ff26ba983d339163e71a4
xqt <info@gno.de>
2017-05-30 08:02:25 +0200

    [IMPR] Make socket_timeout reusable
    On older installations of requests it's not possible to use two timeout values
    but newer versions may have two: one for the connection one for read timeout.
    
    To ensure the right value depending on the requests version there as a fix
    inside http.py; this should be moved to the upper library place to be
    reusable by other library scripts like rcstreams or eventstreams.
    
    __init__.py:
    - Add a new function method to return the socket_timeout dependig on
      requests version but use the higher of the two values.
      Use debug instead of warning because labs is still using older requests
      version 2.2.1.
    
    - http.py:
      remove the socket_timeout fixer and use default_session_timeout() instead.
    
    - Since requests > 2.4.1 is required with T134647 for pwb now
      the default socket_timeout may be a tuple again.
    
    Bug: T166539
    Change-Id: I598ac9fa2fa4f237b68d44bd8cf3c587d5768bd1

--- pywikibot/comms/__init__.py
+++ pywikibot/comms/__init__.py
@@ -1,2 +1,37 @@
 # -*- coding: utf-8 -*-
 """Communication layer."""
+#
+# (C) Pywikibot team, 2017
+#
+# Distributed under the terms of the MIT license.
+#
+from __future__ import absolute_import, unicode_literals
+
+from distutils.version import StrictVersion
+
+from requests import __version__
+
+from pywikibot.config2 import socket_timeout
+from pywikibot.logging import debug
+
+
+_logger = 'comms'
+
+
+def default_socket_timeout():
+    """Return a socket timeout for requests library.
+
+    Older requests library expect a single value whereas newer versions also
+    accept a tuple (connect timeout, read timeout).
+    @return: socket_timeout from config. Either a single float or int for
+        older requests version or a tuple with two items.
+    @rtype: tuple or float or int
+    """
+    if (isinstance(socket_timeout, tuple) and
+            StrictVersion(__version__) < StrictVersion('2.4.0')):
+        debug('The configured timeout is a tuple but requests {0} does not '
+              'support a tuple as a timeout. It uses the higher of the two.'
+              '\nNOTE: requests > 2.4.1 is recommended'
+              .format(__version__), _logger)
+        return max(socket_timeout)
+    return socket_timeout
