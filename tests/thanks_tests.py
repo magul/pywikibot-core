e7930c6b10e45354cc5b24aade3ddb5e1dfbcf3a
Alexander Jones <happy5214@gmail.com>
2017-06-08 18:24:18 -0500

    [WIP] Add on to pywikibot support for thanking normal revisions
    This second commit:
     - Fix the log entry order in thanks_tests
    
    Bug: T135409
    Change-Id: Ib3a35fea2442099fb685ddf0097e6c4003a69fd7

--- tests/thanks_tests.py
+++ tests/thanks_tests.py
@@ -30,7 +30,7 @@ class TestThankRevision(TestCase):
         """
         found_log = can_thank = False
         site = self.get_site()
-        data = site.recentchanges(total=50, reverse=True)
+        data = site.recentchanges(total=20)
         for i in data:
             revid = i['revid']
             username = i['user']
@@ -42,7 +42,8 @@ class TestThankRevision(TestCase):
             self.skipTest('There is no recent change which can be test thanked.')
         before_time = site.getcurrenttimestamp()
         Revision._thank(revid, site, source='pywikibot test')
-        log_entries = site.logevents(logtype='thanks', total=5, start=before_time, page=user)
+        log_entries = site.logevents(logtype='thanks', total=5, start=before_time,
+                                     page=user, reverse=True)
         for __ in log_entries:
             found_log = True
             break
