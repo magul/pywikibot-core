b0579951a63485e031467161d4a5f7bb6ea5ed8a
Tristan Hill <tristan@me.uk.eu.org>
2017-06-08 12:25:46 +0100

    Add family file for beta commons
    Change-Id: I2ce0b9d0bb7be43349cac3aa540ab3c9b6c584da

---
+++ pywikibot/families/betacommons_family.py
@@ -0,0 +1,17 @@
+ -*- coding: utf-8 -*-
+"""Family module for Wikimedia Beta Commons."""
+#
+# (C) Pywikibot team, 2005-2015
+#
+# Distributed under the terms of the MIT license.
+#
+from __future__ import absolute_import, unicode_literals
+
+__version__ = '$Id$'
+
+import commons_family
+
+class Family(commons_family.Family):
+
+    name = 'betacommons'
+    domain = 'commons.wikimedia.beta.wmflabs.org'
