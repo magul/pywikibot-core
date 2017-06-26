de358b903f59248f3b872b1a1135c491ec6903f8
dvorapa <dvorapa@seznam.cz>
2017-06-26 03:00:44 +0200

    [bugfix] Add colon when replacing category and file weblinks
    External links to wiki categories are replaced by internal ones without
    a colon before link target. This behavior is wrong as those internal
    links to categories without a preleading colon are used for a different
    approach.
    
    In addition the problem involves also links to files.
    
    Bug: T127745
    Change-Id: If07182a6476498ad71ced821000bf91df2516cd8

--- tests/cosmetic_changes_tests.py
+++ tests/cosmetic_changes_tests.py
@@ -180,6 +180,25 @@ class TestDryCosmeticChanges(TestCosmeticChanges):
         self.assertEqual(
             '[https://de.wikipedia.org/w/api.php API]',
             self.cct.fixSyntaxSave('[https://de.wikipedia.org/w/api.php|API]'))
+        self.assertEqual(
+            '[[:Category:Example]]\n'
+            '[[:Category:Example|Description]]\n'
+            '[[:File:Example.svg]]\n'
+            '[[:File:Example.svg|Description]]\n'
+            '[[:Category:Example]]\n'
+            '[[:Category:Example|Description]]\n'
+            '[[:File:Example.svg]]\n'
+            '[[:File:Example.svg|Description]]\n',
+            self.cct.fixSyntaxSave(
+                '[https://en.wikipedia.org/wiki/Category:Example]\n'
+                '[https://en.wikipedia.org/wiki/Category:Example Description]\n'
+                '[https://en.wikipedia.org/wiki/File:Example.svg]\n'
+                '[https://en.wikipedia.org/wiki/File:Example.svg Description]\n'
+                '[[https://en.wikipedia.org/wiki/Category:Example]]\n'
+                '[[https://en.wikipedia.org/wiki/Category:Example Description]]\n'
+                '[[https://en.wikipedia.org/wiki/File:Example.svg]]\n'
+                '[[https://en.wikipedia.org/wiki/File:Example.svg Description]]\n'
+            ))
 
     def test_fixHtml(self):
         """Test fixHtml method."""
