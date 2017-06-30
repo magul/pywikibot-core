0ac7a53b1d4b81fb9a15908f5ef4cda5bd46a1ef
Alexander Jones <happy5214@gmail.com>
2017-06-29 19:03:09 -0500

    [WIP] Add ability to thank Flow posts
    I still need to write the tests for this code.
    
    Bug: T135411
    Change-Id: Ie629aaa46aa007a39e6d5dce6400504bf73e5980

--- tests/thanks_tests.py
+++ tests/thanks_tests.py
@@ -7,12 +7,14 @@
 #
 from __future__ import absolute_import, unicode_literals
 
+from pywikibot.flow import Topic
 from pywikibot.page import Page, Revision, User
 
 from tests.aspects import TestCase
 
 
 NO_THANKABLE_REVS = 'There is no recent change which can be test thanked.'
+NO_THANKABLE_POSTS = 'There is no recent post which can be test thanked.'
 
 
 class TestThankRevision(TestCase):
@@ -105,3 +107,54 @@ class TestThankRevisionErrors(TestCase):
         for invalid_revid in invalid_revids:
             self.assertAPIError('invalidrevision', None, Revision._thank,
                                 invalid_revid, site, source='pywikibot test')
+
+
+class TestThankFlowPost(TestCase):
+
+    """Test thanks for Flow posts."""
+
+    family = 'test'
+    code = 'test'
+
+    write = True
+
+    def test_thank_post(self):
+        """Test thanks for Flow posts."""
+        self.skipTest('Users not yet found in Post.')
+        found_log = False
+        site = self.get_site()
+        data = site.randompages(namespaces='Topic', total=20)
+        for page in data:
+            topic = Topic(page)
+            post = topic.root
+            # user = User(site, post.creator)
+            # if user.is_thankable:
+            if True:
+                break
+        else:
+            self.skipTest(NO_THANKABLE_REVS)
+        before_time = site.getcurrenttimestamp()
+        post.thank()
+        log_entries = site.logevents(logtype='thanks', total=5,  # page=user,
+                                     start=before_time, reverse=True)
+        for __ in log_entries:
+            found_log = True
+            break
+        self.assertTrue(found_log)
+
+    def test_self_thank(self):
+        """Test that thanking one's own Flow post causes an error."""
+        self.skipTest('Users not yet found in Post.')
+        site = self.get_site()
+        my_name = self.get_userpage().username
+        data = site.usercontribs(user=my_name, total=1)
+        for rev in data:
+            revid = rev['revid']
+            break
+        else:
+            test_page = Page(site, 'Pywikibot Thanks test')
+            test_page.text += '* ~~~~\n'
+            test_page.save('Pywikibot Thanks test')
+            revid = test_page.latest_revision_id
+        self.assertAPIError('invalidrecipient', None, Revision._thank,
+                            revid, site, source='pywikibot test')
