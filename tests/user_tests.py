1a65bd3d6b1048ad65658c253f403820145ded28
xqt <info@gno.de>
2017-07-01 14:11:43 +0200

    [bugfix] Don't fail when creating User class for autoblock id
    - Creating a user class with autoblock ID fails with InvalidTitle when a
      namespace identifier is given in front of the ID. It works without that
      preleading namespaces.
    - This patch solves that issue and is needed handling BlockEvents in a
      proper way.
    - A more general solution with a different Autoblock class proposed by
      b4f0bdd8 may come later.
    - tests added
    
    Bug: T108515
    Change-Id: I6de75bea5c3287cca04700b230fdec881189f32c

--- tests/user_tests.py
+++ tests/user_tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Tests for the User page."""
 #
-# (C) Pywikibot team, 2016
+# (C) Pywikibot team, 2016-2017
 #
 # Distributed under the terms of the MIT license.
 #
@@ -9,6 +9,7 @@ from __future__ import absolute_import, unicode_literals
 
 import pywikibot
 
+from pywikibot.exceptions import AutoblockUser
 from pywikibot import User
 
 from tests.aspects import TestCase, unittest
@@ -76,6 +77,40 @@ class TestUserClass(TestCase):
         self.assertIsNone(user.registration())
         self.assertFalse(user.isEmailable())
         self.assertIn('invalid', user.getprops())
+
+    def test_autoblocked_user(self):
+        """Test autoblocked user."""
+        user = User(self.site, '#1242976')
+        self.assertEqual('#1242976', user.username)
+        self.assertEqual(user.name(), user.username)
+        self.assertEqual(user.title(withNamespace=False), user.username[1:])
+        self.assertFalse(user.isRegistered())
+        self.assertFalse(user.isAnonymous())
+        self.assertIsNone(user.registration())
+        self.assertFalse(user.isEmailable())
+        self.assertIn('invalid', user.getprops())
+        self.assertTrue(user._isAutoblock)
+        self.assertRaisesRegex(AutoblockUser, 'This is an autoblock ID',
+                               user.getUserPage)
+        self.assertRaisesRegex(AutoblockUser, 'This is an autoblock ID',
+                               user.getUserTalkPage)
+
+    def test_autoblocked_user_with_namespace(self):
+        """Test autoblocked user."""
+        user = User(self.site, 'User:#1242976')
+        self.assertEqual('#1242976', user.username)
+        self.assertEqual(user.name(), user.username)
+        self.assertEqual(user.title(withNamespace=False), user.username[1:])
+        self.assertFalse(user.isRegistered())
+        self.assertFalse(user.isAnonymous())
+        self.assertIsNone(user.registration())
+        self.assertFalse(user.isEmailable())
+        self.assertIn('invalid', user.getprops())
+        self.assertTrue(user._isAutoblock)
+        self.assertRaisesRegex(AutoblockUser, 'This is an autoblock ID',
+                               user.getUserPage)
+        self.assertRaisesRegex(AutoblockUser, 'This is an autoblock ID',
+                               user.getUserTalkPage)
 
 
 if __name__ == '__main__':  # pragma: no cover
