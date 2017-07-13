64c2736aca841b86cd1f6ce5d6dfe1c11639ea56
xqt <info@gno.de>
2017-07-13 11:00:37 +0200

    [L10N] Update languages_by_size
    Change-Id: I26155efe8706b85a8865412235d8150a8042a4c4

--- pywikibot/families/wikibooks_family.py
+++ pywikibot/families/wikibooks_family.py
@@ -40,8 +40,8 @@ class Family(family.SubdomainFamily, family.WikimediaFamily):
             'en', 'hu', 'de', 'fr', 'ja', 'it', 'es', 'pt', 'nl', 'pl', 'he',
             'vi', 'ca', 'th', 'fi', 'id', 'sq', 'fa', 'zh', 'ru', 'cs', 'az',
             'sv', 'da', 'hr', 'sr', 'tr', 'ko', 'ar', 'no', 'gl', 'ro', 'ta',
-            'tl', 'mk', 'is', 'sa', 'uk', 'ka', 'lt', 'tt', 'eo', 'hi', 'sk',
-            'el', 'bg', 'bn', 'hy', 'li', 'si', 'ms', 'sl', 'ur', 'la', 'ml',
+            'tl', 'mk', 'is', 'uk', 'sa', 'hi', 'ka', 'lt', 'tt', 'eo', 'sk',
+            'el', 'bg', 'bn', 'li', 'hy', 'si', 'ms', 'sl', 'ur', 'la', 'ml',
             'km', 'ia', 'et', 'cv', 'mr', 'eu', 'oc', 'kk', 'be', 'pa', 'ne',
             'fy', 'tg', 'te', 'af', 'ku', 'ky', 'bs', 'mg', 'cy',
         ]
