410dcbdd06e90d16bfb454579b7dec2447937188
Zoranzoki21 <zorandori4444@gmail.com>
2017-12-17 13:36:37 +0100

    Create family file for Wikimania wikis
    Bug: T182982
    Change-Id: Idefa0f9c8ca91dbe9b2f350c0db68062eab17746

---
+++ pywikibot/families/wikimania_family.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""Family module for Wikimania wikis."""
+#
+# (C) Pywikibot team, 2017
+#
+# Distributed under the terms of the MIT license.
+#
+from __future__ import absolute_import, unicode_literals
+
+from pywikibot import family
+from pywikibot.tools import deprecated
+
+
+class Family(family.SubdomainFamily, family.WikimediaFamily):
+
+    """Family for Wikimania wikis hosted on wikimedia.org."""
+
+    name = 'wikimania2018'
+    code = 'en'
+
+    def protocol(self, code):
+        """Return the protocol for this family."""
+        return 'https'
