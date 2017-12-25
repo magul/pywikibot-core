7ce1b4a1929544019c488eb4a5459fa0c2d4131d
David Sn <divad.nnamtdeis@gmail.com>
2017-12-26 00:01:01 +0100

    Use symlink instead of copyfile in download_dump.py
    Bug: T183668
    Change-Id: I6491aed72e292260e0c464d3e7eb87e739402878

--- scripts/maintenance/download_dump.py
+++ scripts/maintenance/download_dump.py
@@ -22,7 +22,8 @@ import io
 import os.path
 import sys
 
-from shutil import copyfile, copyfileobj
+from os import symlink
+from shutil import copyfileobj
 
 import pywikibot
 
@@ -72,7 +73,8 @@ class DownloadDumpBot(Bot):
             self.getOption('wikiname'), self.getOption('filename'))
         if toolforge_dump_filepath:
             pywikibot.output('Copying file from ' + toolforge_dump_filepath)
-            copyfile(toolforge_dump_filepath, file_storepath)
+            if not os.path.exists(file_storepath):
+                symlink(toolforge_dump_filepath, file_storepath)
         else:
             url = 'https://dumps.wikimedia.org/' + \
                 os.path.join(self.getOption('wikiname'),
