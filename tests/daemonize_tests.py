7dba5f01b578d23273bd1b2b82c49d014a637326
Ricky Setiawan <greenzone332@outlook.com>
2017-12-29 00:44:48 +0700

    Creating daemonize_tests.py
    Change-Id: Idc867df606a8cfa2d392d41b67710bed2f3570b5

--- tests/daemonize_tests.py
+++ tests/daemonize_tests.py
@@ -1,28 +1,46 @@
 # -*- coding: utf-8 -*-
 
+"""
+Test for daemonize
+"""
+
 from __future__ import absolute_import, unicode_literals
 
 from pywikibot import daemonize
 
 import os
 
+
 def test_daemonize_os_exit():
     os.fork()
     daemonize.daemonize()
     daemonize.daemonize()
+
+
 def test_daemonize_closedstream_true():
     daemonize.daemonize(True, True, False, None)
+
+
 def test_daemonize_closedstream_false():
     daemonize.daemonize(False, True, False, None)
+
+
 def test_daemonize_changedirectory_true():
     daemonize.daemonize(True, True, False, None)
+
+
 def test_daemonize_changeddirectory_false():
     daemonize.daemonize(True, False, False, None)
+
+
 def test_redirectstd_true():
     daemonize.daemonize(True, True, False, 'Test')
+
+
 def test_daemonize_writepid_True():
     os.fork()
     # cannot be tested because spaghetti code
+
 
 def load_tests(loader, tests, pattern):
     test_daemonize_os_exit()
