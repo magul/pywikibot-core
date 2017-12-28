03c00ce977aa37cdca6bcba441a8791104e56787
Ricky Setiawan <greenzone332@outlook.com>
2017-12-28 23:42:11 +0700

    Creating daemonzie_tests.py
    Change-Id: I012da1d7740990f072337b0525b5873c35b0c16f

--- tests/daemonize_tests.py
+++ tests/daemonize_tests.py
@@ -1,30 +1,33 @@
-import pywikibot
 from pywikibot import daemonize
 import os
 
 def test_daemonize_os_exit():
-   os.fork()
-   daemonize.daemonize()
+    os.fork()
+    daemonize.daemonize()
+    daemonize.daemonize()
 def test_daemonize_closedstream_true():
-   daemonize.daemonize(True, True, False, None)
+    daemonize.daemonize(True, True, False, None)
 
 def test_daemonize_closedstream_false():
-   daemonize.daemonize(False, True, False, None)
+    daemonize.daemonize(False, True, False, None)
 
 def test_daemonize_changedirectory_true():
-   daemonize.daemonize(True, True, False, None)
+    daemonize.daemonize(True, True, False, None)
 
-def test_daemonize_changeddirectory_false():
-   daemonize.daemonize(True, False, False, None)
+def test_daemonize_changeddirectory_false(): 
+    daemonize.daemonize(True, False, False, None)
 
+def test_redirectstd_true():
+    daemonize.daemonize(True, True, False, "Test")
 def test_daemonize_writepid_True():
-   os.fork()
-   """cannot be tested because spaghetti code"""
+    os.fork()
+    #cannot be tested because spaghetti code
 
-def load_tests(loader, tests, pattern):
-   test_daemonize_os_exit()
-   test_daemonize_closedstream_true()
-   test_daemonize_closedstream_false()
-   test_daemonize_changedirectory_true()
-   test_daemonize_changeddirectory_false()
-   test_daemonize_writepid_True()
+def load_tests(loader, tests, pattern): 
+    test_daemonize_os_exit()
+    test_daemonize_closedstream_true()
+    test_daemonize_closedstream_false()
+    test_daemonize_changedirectory_true()
+    test_daemonize_changeddirectory_false()
+    test_daemonize_writepid_True()
+    test_redirectstd_true()
