261dbc0e92ff9ee1dac5054add6b4e129ae494d5
Ricky Setiawan <greenzone332@outlook.com>
2017-12-30 12:41:17 +0700

    Create editor_tests.py
    Change-Id: I708c34cc52b62b5fd80d72384fb28eb21aaa5e33

--- tests/editor_tests.py
+++ tests/editor_tests.py
@@ -12,13 +12,13 @@ from pywikibot import editor
 
 
 def test_editor_edituserconfigpy_same():
-    editor.edit(text=None, jumpIndex=200, highlight='test1')
+    editor.TextEditor.edit(text=None, jumpIndex=200, highlight='test1')
 
 
 def test_editor_edituserconfigpy():
-    editor.edit(text='test1', jumpIndex=200, highlight='test1')
+    editor.TextEditor.edit(text='test1', jumpIndex=200, highlight='test1')
 
 
 def test_editor_command():
     tempfile.TemporaryFile(suffix='test', prefix='txt', dir=os.getcwd())
-    editor.command(tempFilename=os.getcwd() + 'test.txt', text='test')
+    editor.TextEditor.command(tempFilename=os.getcwd() + 'test.txt', text='test')
